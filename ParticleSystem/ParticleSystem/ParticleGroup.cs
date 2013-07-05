c86d066e849983b84feb416e9e3c3eea2a212fde
Grzegorz Marcinkowski <grzemarcinkowski@gmail.com>
2013-07-05 15:04:52 +0200

    created particle group

---
+++ ParticleSystem/ParticleSystem/ParticleGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParticleSystem
+{
+    //TODO
+    public abstract class ParticleGroup
+    {
+
+        protected virtual void SetupParameters()
+        {
+        }
+
+        protected virtual void InitializeAtom(Particle particle, Vector2 location)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
