790047abaa21a41e27c31cb381ed45a6048ce6c3
Grzegorz Marcinkowski <grzemarcinkowski@gmail.com>
2013-07-07 21:17:54 +0200

    particle group update method

--- ParticleSystem/ParticleSystem/ParticleGroup.cs
+++ ParticleSystem/ParticleSystem/ParticleGroup.cs
@@ -12,23 +12,51 @@ namespace ParticleSystem
     //TODO
     public abstract class ParticleGroup
     {
+        protected Queue<Particle> freeParticles;
+        protected List<Particle> particles;
+
+        public Texture2D Texture
+        {
+            get;
+            private set;
+        }
+
+        public ParticleGroup()
+        {
+            freeParticles = new Queue<Particle>();
+            particles = new List<Particle>();
+        }
+
+        public void LoadContent(ContentManager content, string fileName)
+        {
+            Texture = content.Load<Texture2D>(fileName);  
+        }
 
         protected virtual void SetupParameters()
         {
         }
 
-        protected virtual void InitializeAtom(Particle particle, Vector2 location)
+        protected virtual void InitializeParticle(Particle particle, Vector2 location)
         {
         }
 
         public void Update(GameTime gameTime)
         {
-
+            float time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            foreach (Particle p in particles)
+            {
+                p.Update(time);
+                if (!(p.Age < p.Lifetime))
+                {
+                    freeParticles.Enqueue(p);
+                    p.Reset();
+                }
+            }
+            particles.RemoveAll( (item) => !(item.Age < item.Lifetime));
         }
 
         public virtual void Draw(SpriteBatch spriteBatch)
         {
-
         }
 
     }
